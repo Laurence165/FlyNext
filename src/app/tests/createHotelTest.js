ad772cc244c4185bcc1cdf9494f1389ff1039c85
liulaur3 <laurenceliu.liu@mail.utoronto.ca>
2025-04-04 16:21:11 -0400

    t

--- src/app/tests/createHotelTest.js
+++ src/app/tests/createHotelTest.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL + "/api" || "http://localhost:3000/api";
-
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL + "/api" || "/api"
 async function testCreateHotel() {
   try {
     const testHotelData = {
